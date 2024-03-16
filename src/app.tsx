63156a84a33e6d870924d24425f78b0e9c789cd3
Naibuu <hawkwer141099@gmail.com>
2024-03-16 19:09:19 +0200

    remove: timeline

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 /** Components */
 import Layout from '~components/layout'
 
 /** Routes */
 import { HomePage } from '~routes/home'
-import { TimelinePage } from '~routes/timeline'
 import { WorkPage } from '~routes/work'
 import { ErrorRedirect } from '~routes/errorRedirect'
 
 export default function App() {
     return (
         <BrowserRouter>
+            <Toaster />
             <Layout>
                 <Routes>
                     <Route path="/" element={<HomePage />} />
-                    <Route path="/timeline" element={<TimelinePage />} />
                     <Route path="/work" element={<WorkPage />} />
                     <Route path="*" element={<ErrorRedirect />} />
                 </Routes>
